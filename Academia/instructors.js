ca3e6259cd33b918aefb1683b7b972aa1b8222f7
albertohass <alberto.hass@gmail.com>
2020-05-13 20:39:18 -0300

    adicionado tela de updates

--- Academia/instructors.js
+++ Academia/instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require("./data.json")
+const { age, date } = require("./utils.js")
 
 //show
 exports.show = function(req,res) {
@@ -13,13 +14,14 @@ exports.show = function(req,res) {
 
     if (!foundInstructor) return res.send("Instructor not found")
     
+
     const instructor = {
         ...foundInstructor,
-        age: "",    
+        age: age(foundInstructor.birth),    
         services: foundInstructor.services.split(","),
-        created_at: ""
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
-
+     
     return res.render("instructors/show", {instructor})
 }
 
@@ -56,6 +58,26 @@ exports.post = function(req, res){
         return res.redirect("/instructors")
     })
 }
-// update
+
+// edit
+exports.edit = function (req, res){ 
+    
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    
+
+    res.render("instructors/edit", {instructor})
+}
 
 //delete
