448b281148587ea8986a73d732d75450987cff79
Alberto Hass <50429189+albertohass@users.noreply.github.com>
2020-05-18 13:05:47 -0300

    Add files via upload

---
+++ instructors.js
@@ -0,0 +1,135 @@
+const fs = require('fs')
+const data = require("./data.json")
+const { age, date } = require("./utils.js")
+
+exports.index = function(req, res){
+    return res.render("instructors/index", {instructors: data.instructors})
+}
+
+
+//show
+exports.show = function(req,res) {
+    // req.query.id case ?id = xxx
+    // req.body case post 
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+    
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),    
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+    }
+     
+    return res.render("instructors/show", {instructor})
+}
+
+// create
+exports.post = function(req, res){
+    
+    const keys = Object.keys(req.body)
+
+    for(key of keys){
+        if (req.body[key] == ""){   
+            return res.send("Preencha todos os campos")
+        }   
+    }
+    
+    let {avatar_url, birth, name, services, gender} = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number (data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        name,
+        birth,
+        avatar_url,
+        gender,
+        services,
+        created_at,
+    })
+    
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error')
+
+        return res.redirect("/instructors")
+    })
+}
+
+// edit
+exports.edit = function (req, res){ 
+    
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    
+
+    res.render("instructors/edit", {instructor})
+}
+
+// put
+
+exports.put = function(req, res){
+    
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if (instructor.id == id){
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send("Usuario não identificado")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id:Number(req.body.id)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json",JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("white error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+//delete
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredInstructor = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) res.send("Error")
+
+        return res.redirect("/instructors")
+    })
+}
---
+++ routes.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const routes = express.Router()
+const instructors = require("./instructors")
+
+routes.get("/",function(req, res){
+    return res.redirect("/instructors")
+})
+
+routes.get("/instructors",instructors.index)
+
+routes.get("/instructors/create",function(req, res){
+    return res.render("instructors/create")
+})
+
+routes.get("/instructors/:id/edit",instructors.edit)
+
+routes.get("/instructors/:id",instructors.show)
+
+routes.post("/instructors",instructors.post)
+
+routes.put("/instructors", instructors.put)
+
+routes.delete("/instructors", instructors.delete)
+
+routes.get("/members",function(req, res){
+    return res.render("members")
+})
+
+module.exports = routes
+
+// HTTP VERBS
+// GET: Receber RESOURCE
+// POST : Criar um novo RESOURCE com dados enviados 
+// PUT : Atualizar RESOURCE     
+// DELETE : deletar RESOURCE
